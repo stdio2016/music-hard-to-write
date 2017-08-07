0625c7fe3697c36ec9da14442eeb4a10fa425caa
stdio2016 <randomfrank8@gmail.com>
2017-08-08 07:21:23 +0800

    created during midterm

---
+++ midterm/createmp4.bat
@@ -0,0 +1,1 @@
+ffmpeg -loop 1 -f image2 -i "musichardwrite.png" -i C:\Users\User\Documents\MuseScore2\Scores\音樂期中作業\陳羿豐.wav -c:v libx264 -c:a aac -strict experimental -b:a 192k -t 00:01:19.000 -pix_fmt yuv420p music.mp4
